77d0ed34061646aa191dca2ee4579b9b724a2f0f
CarolCheng <yuntingcheng@gmail.com>
2016-12-30 17:29:50 +0800

    Create netcommon.h

---
+++ A TCP Echo Client and Server/netcommon.h
@@ -0,0 +1,9 @@
+#ifndef NETCOMMON_HPP 
+#define NETCOMMON_HPP
+#include <memory>
+#include <boost/asio.hpp>
+
+typedef boost::asio::ip::tcp TCP;
+const int max_size = 1024;
+
+#endif // NETCOMMON_HPP
