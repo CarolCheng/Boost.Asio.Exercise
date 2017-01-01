6fd1b7b1be08303b761a8272819a4f59b7416e54
CarolCheng <yuntingcheng@gmail.com>
2017-01-01 14:50:28 +0800

    correct code style

--- A TCP Echo Client and Server/sync_tcp_echo_client.cpp
+++ A TCP Echo Client and Server/sync_tcp_echo_client.cpp
@@ -7,7 +7,7 @@
 class sync_echo_client 
 {
 public:
-	sync_echo_client(boost::asio::io_service& io_service)
+	explicit sync_echo_client(boost::asio::io_service& io_service)
 		:socket_(io_service) 
 	{
 		
@@ -21,12 +21,12 @@ public:
 	{
 		char buffer[max_size];
 		socket_.write_some(boost::asio::buffer(msg + "\n"));
-		int bytes = boost::asio::read(
-		socket_, boost::asio::buffer(buffer), 
-		[&](boost::system::error_code /*ec*/, std::size_t bytes) ->size_t
-		{
-			return on_read(buffer, bytes);
-		});
+		int bytes = boost::asio::read(socket_, boost::asio::buffer(buffer), 
+			[&](boost::system::error_code /*ec*/, std::size_t bytes) ->size_t
+			{
+				return on_read(buffer, bytes);
+			});
+		// print : whether or not the sent and received message is same 
 		std::string copy(buffer, bytes - 1);
 		std::cout << "server echoed our " << msg<< ": "
 		          << (copy == msg ? "OK" : "FAIL") << std::endl;
@@ -40,7 +40,6 @@ private:
 	}
 private:
 	TCP::socket socket_;
-	char buffer[max_size];
 };
 int main(int argc, char* argv[]) 
 {
@@ -54,15 +53,13 @@ int main(int argc, char* argv[])
 	// Launch a group of client threads
 	std::vector<std::thread> workers;
 	for(auto p : messages) {
-		workers.push_back(std::thread( [&]{
+		workers.push_back(std::thread( [&] {
 			sync_echo_client client(service);
 			client.connect("127.0.0.1", 4403);
 			client.start(p);
-		})
-		);
+		}));
 		std::this_thread::sleep_for(std::chrono::seconds(1));
 	}
-
 	// Join the threads with the main thread
 	std::for_each(workers.begin(), workers.end(), [](std::thread &t) 
 	{
